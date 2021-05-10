e74a15ff1743f4e6510f5c2826faacee9885b7cc
ik373321 <82790121+ik373321@users.noreply.github.com>
2021-05-10 10:57:57 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,69 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
+
+  def index
+    query = "SELECT * FROM prototypes"
+    @prototype = Prototype.find_by_sql(query)
+
+
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    #binding.pry
+    @prototype = Prototype.new(prototypes_params)
+    #binding.pry
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  def show
+   @prototype = Prototype.find(params[:id])
+   @comment = Comment.new
+   @comments = @prototype.comments.includes(:user)
+
+    
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+
+    if @prototype.update(prototypes_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  private
+
+  def prototypes_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  
+end
